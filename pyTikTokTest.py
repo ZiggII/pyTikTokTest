eec211353719f5729b22804be9cc2246c70787c7
ZiggII <zdeno@sekerak.eu>
2022-12-18 17:05:45 +0100

    chnage

--- pyTikTokTest.py
+++ pyTikTokTest.py
@@ -1,2 +1,11 @@
 from pytiktok import Tikvideo
 
+Video = Tikvideo()
+
+
+k = Video.tiktok("https://www.tiktok.com/@priyakaede/video/7173389471017110790?is_from_webapp=1&sender_device=pc0",True)
+
+print(k["ok"])
+
+print(k["link"])
+
