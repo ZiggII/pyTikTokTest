d2ebf776cefdd9ff05f1043087b8896c6e011cb7
ZiggII <zdenovsekerak.eu>
2022-12-30 15:11:33 +0100

    chnage

--- pyTikTokTest.py
+++ pyTikTokTest.py
@@ -1,5 +1,9 @@
 import requests
 
-request = requests.get("https://www.tiktok.com/")
+headers_by_me = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+    'referer': "https://www.google.com/"
+    }
+request = requests.get("https://www.tiktok.com/", headers= headers_by_me)
 
 print(request)
