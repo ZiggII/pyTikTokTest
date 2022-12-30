0227a2f2456a8ae7bbe8233c3a2454effb8559cd
ZiggII <zdenovsekerak.eu>
2022-12-30 15:11:16 +0100

    change

--- pyTikTokTest.py
+++ pyTikTokTest.py
@@ -1,11 +1,5 @@
-from pytiktok import Tikvideo
+import requests
 
-Video = Tikvideo()
+request = requests.get("https://www.tiktok.com/")
 
-
-k = Video.tiktok("https://www.tiktok.com/@priyakaede/video/7173389471017110790?is_from_webapp=1&sender_device=pc0",True)
-
-print(k["ok"])
-
-print(k["link"])
-
+print(request)
